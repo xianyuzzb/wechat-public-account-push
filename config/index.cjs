778b9c42c6c72f92e411922e47067b2460f08bb4
xianyuzzb <65759503+xianyuzzb@users.noreply.github.com>
2023-11-13 17:25:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'OgW2B4lsLWIjP8xyDaZsgJ-fjcNc4PaBkR0kQvfoWE0',
+  // CALLBACK_TEMPLATE_ID: 'OgW2B4lsLWIjP8xyDaZsgJ-fjcNc4PaBkR0kQvfoWE0',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      // name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oPh6d6M9gemx3-YnXVjFUF3hNKFE',
+      // id: 'oPh6d6M9gemx3-YnXVjFUF3hNKFE',
     }
   ],
 
