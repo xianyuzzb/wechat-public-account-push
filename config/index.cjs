ba667d50189c06f513b91f62ce38f93da01499e7
xianyuzzb <65759503+xianyuzzb@users.noreply.github.com>
2023-11-14 10:33:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oPh6d6M9gemx3-YnXVjFUF3hNKFE','oPh6d6G8dnB9l_8Jd2HNenZ-1iu8',
+      id: 'oPh6d6G8dnB9l_8Jd2HNenZ-1iu8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '6JHQoWHY7h9rzlQpgWADBZl6IRIodq1F8u846qksKyU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      // name: '自己',
+      name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // id: 'oPh6d6M9gemx3-YnXVjFUF3hNKFE',
+      id: 'oPh6d6M9gemx3-YnXVjFUF3hNKFE',
     }
   ],
 
